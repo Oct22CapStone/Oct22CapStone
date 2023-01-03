5604c759d11f72656b943f00624c3b623c93a036
phuonghhc <116579412+phuonghhc@users.noreply.github.com>
2023-01-03 10:15:38 -0600

    Added more products in sql

--- Add_Products.sql
+++ Add_Products.sql
@@ -18,84 +18,28 @@ INSERT INTO product ( show_product, product_qty, product_name, productimg, price
 VALUES (1, 10, "Strawberry", "https://hips.hearstapps.com/clv.h-cdn.co/assets/15/22/1432664914-strawberry-facts1.jpg?resize=480:*", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Banana", "https://foodandnutrition.org/wp-content/uploads/SavorBananas.jpg", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Orange", "https://cdn.britannica.com/24/174524-050-A851D3F2/Oranges.jpg", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Red Apple", "https://minnetonkaorchards.com/wp-content/uploads/2021/04/Bright-Red-Apple.jpg.webp", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Green Apple", "https://media.istockphoto.com/id/584226186/photo/green-apple.jpg?b=1&s=170667a&w=0&k=20&c=lDC_bfwsTi7_B_TdFClMNLUoYzVK331rOo_KECwby1U=", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Yellow Apple", "https://i.pinimg.com/736x/de/52/3a/de523a917681186a5119534aa3ce9126--stock-photo-sites-free-stock-photo.jpg", 20.00, "Product Description");
 
 INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "name", "image", 20.00, "Product Description");
+VALUES (1, 10, "Mango", "https://exoticfruitbox.com/wp-content/uploads/2015/10/mango.jpg", 20.00, "Product Description");
 
-/**
+INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
+VALUES (1, 10, "Green Grapes", "image", 20.00, "https://www.meijer.com/content/dam/meijer/product/0000/00/0004/02/0000000004022_2_A1C1_0600.png");
 
-{
-        "productQty": 12,
-        "productName": "Banana",
-        "productImg": "https://foodandnutrition.org/wp-content/uploads/SavorBananas.jpg",
-        "price_per_unit": 3.00,
-        "productDescription": "This is a Banana"
-    }, 
-    {
-        "productQty": 12,
-        "productName": "Orange",
-        "productImg": "https://cdn.britannica.com/24/174524-050-A851D3F2/Oranges.jpg",
-        "price_per_unit": 2.00,
-        "productDescription": "This is an Orange"
-    }, 
-    {
-        "productQty": 3,
-        "productName": "Red Apple",
-        "productImg": "https://minnetonkaorchards.com/wp-content/uploads/2021/04/Bright-Red-Apple.jpg.webp",
-        "price_per_unit": 5.00,
-        "productDescription": "This is an Apple"
-    }, 
-    {
-        "productQty": 10,
-        "productName": "Green Apple",
-        "productImg": "https://media.istockphoto.com/id/584226186/photo/green-apple.jpg?b=1&s=170667a&w=0&k=20&c=lDC_bfwsTi7_B_TdFClMNLUoYzVK331rOo_KECwby1U=",
-        "price_per_unit": 5.00,
-        "productDescription": "This is a Green Apple"
-    }, 
-    {
-        "productQty": 12,
-        "productName": "Yellow Apple",
-        "productImg": "https://i.pinimg.com/736x/de/52/3a/de523a917681186a5119534aa3ce9126--stock-photo-sites-free-stock-photo.jpg",
-        "price_per_unit": 4.00,
-        "productDescription": "This is a Yellow Apple"
-    }, 
-    {
-        "productQty": 12,
-        "productName": "Mango",
-        "productImg": "https://exoticfruitbox.com/wp-content/uploads/2015/10/mango.jpg",
-        "price_per_unit": 5.00,
-        "productDescription": "This is a Mango"
-    }, 
-    {
-        "productQty": 12,
-        "productName": "Green Grapes",
-        "productImg": "https://www.meijer.com/content/dam/meijer/product/0000/00/0004/02/0000000004022_2_A1C1_0600.png",
-        "price_per_unit": 10.00,
-        "productDescription": "This are Green Grapes"
-    }, 
-    {
-        "productQty": 12,
-        "productName": "Purple Grapes",
-        "productImg": "https://images.heb.com/is/image/HEBGrocery/000375267",
-        "price_per_unit": 10.00,
-        "productDescription": "This are Purple Grapes"
-    }
+INSERT INTO product ( show_product, product_qty, product_name, productimg, price_per_unit, productdesc)
+VALUES (1, 10, "Purple Grapes", "https://images.heb.com/is/image/HEBGrocery/000375267", 20.00, "Product Description");
 
-    
-]
 
-**/
 
