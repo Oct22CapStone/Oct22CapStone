3605ada8a296157cff3c54f8d68a6d634dff4d17
paramshahhcl <119872511+paramshahhcl@users.noreply.github.com>
2023-01-06 08:32:25 -0600

    Delete src directory

--- src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://localhost:8181",
-    headers: {
-        "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-    },
-});
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
