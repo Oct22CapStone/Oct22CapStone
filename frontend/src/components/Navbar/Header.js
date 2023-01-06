0692950f6d171ef32cc9a49f0f1b4f73771c1451
chinar97 <120409366+chinar97@users.noreply.github.com>
2023-01-06 17:29:23 -0500

    new commit to fix header

--- frontend/src/components/Navbar/Header.js
+++ frontend/src/components/Navbar/Header.js
@@ -4,11 +4,11 @@ function Header (){
     return (
         <div>
 
-        <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
-                <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">Browse the latest trends and essentials</p>
+        <header className="bg-dark py-5">
+            <div className="container px-4 px-lg-5 my-5">
+                <div className="text-center text-white">
+                    <h1 className="display-4 fw-bolder">Shop in style</h1>
+                    <p className="lead fw-normal text-white-50 mb-0">Browse the latest trends and essentials</p>
                 </div>
             </div>
         </header>
