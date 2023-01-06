0d2e2ab82a7227e80778c8c1a98122edd7726acf
paramshahhcl <119872511+paramshahhcl@users.noreply.github.com>
2023-01-06 08:32:05 -0600

    Delete Add_Products.sql

--- Add_Products.sql
+++
@@ -1,5 +0,0 @@
-/** TESTING.... Add Products **/
-
-INSERT INTO product ( show_product, productQty, product_name, productimg, price_per_unit, productdesc)
-VALUES (1, 10, "Durian", "https://healthiersteps.com/wp-content/uploads/2021/09/durian-1.jpg", 20.00, "The best stinky fruit ever");
-
