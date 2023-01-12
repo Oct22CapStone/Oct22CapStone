e088a678d7838d578f9a059044563479e983c301
StjWorld <st_murat@hotmail.com>
2023-01-12 17:55:36 -0600

    demo preperation

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export const oktaConfig = {
 
-    issuer: 'https://dev-38245936.okta.com/oauth2/default',//chinar's issuer
-    clientId: "0oa7urjx6kd5LTbMZ5d7",   //chinar's clientId
+    issuer: 'https://dev-09055718.okta.com/oauth2/default',//chinar's issuer
+    clientId: "0oa7qou2xcRLl9WvU5d7",   //chinar's clientId
 
     redirectUri: `${window.location.origin}/login/callback`,
     //redirectUri: 'http://localhost:3000/login/callback',
--- frontend/src/pages/ViewProducts.js
+++ frontend/src/pages/ViewProducts.js
@@ -23,6 +23,7 @@ const ViewProducts = () => {
 			} catch(error) {
 				console.log(error);
 			}
+		
 			setLoading(false);
 		};
 		fetchData();
