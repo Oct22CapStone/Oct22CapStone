eafb75d89a84d4d8a97ee2a17d29f72fd838e411
tanb98 <103166315+tanb98@users.noreply.github.com>
2023-01-12 17:17:27 -0600

    functional delete button

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export const oktaConfig = {
 
-    issuer: 'https://dev-38245936.okta.com/oauth2/default',//chinar's issuer
-    clientId: "0oa7urjx6kd5LTbMZ5d7",   //chinar's clientId
+    issuer: 'https://dev-75726251.okta.com/oauth2/default',//Tanner's issuer
+    clientId: "0oa7ubwwdkshNMbWg5d7",   //Tanner's clientId
 
     redirectUri: `${window.location.origin}/login/callback`,
     //redirectUri: 'http://localhost:3000/login/callback',
