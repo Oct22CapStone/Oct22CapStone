04299691966c480f08b9c263f12a391a5e536351
phuonghhc <116579412+phuonghhc@users.noreply.github.com>
2023-01-15 10:34:02 -0600

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,11 @@
+
 export const oktaConfig = {
 
-    issuer: 'https://dev-89041717.okta.com/oauth2/default',//chinar's issuer
-    clientId: "0oa7w713t1hMkgPG15d7",   //chinar's clientId
-
+    //dev-09055718.okta.com/oauth2/default',
+    issuer: 'https://dev-97900310.okta.com/oauth2/default',
+    //clientId: "0oa7qou2xcRLl9WvU5d7",
+    clientId: "0oa7sq1asxXKBblPa5d7", // SINGLE PAGE APP - WORKS
+    //clientId: "0oa7spieo4luUqayk5d7", WEB APP - DOESN'T WORK
     redirectUri: `${window.location.origin}/login/callback`,
     //redirectUri: 'http://localhost:3000/login/callback',
     endSessionRedirectUri: `${window.location.origin}`,
