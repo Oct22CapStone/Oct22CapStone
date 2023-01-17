9af68deca213f2b5f82350f1bf301b97d457471e
chinar97 <120409366+chinar97@users.noreply.github.com>
2023-01-17 18:55:36 -0500

    Update ProductService.js

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PRODUCT_API_BASE_URL = "http://localhost:8181/product";
+const PRODUCT_API_BASE_URL = "https://backendecommerce.azurewebsites.net/product";
 
 
 class ProductService {
