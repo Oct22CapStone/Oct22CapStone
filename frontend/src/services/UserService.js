2e38bfdd73b1e10d21bd48f7c12610f9c11644d6
chinar97 <120409366+chinar97@users.noreply.github.com>
2023-01-17 18:54:37 -0500

    Update UserService.js

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -10,28 +10,28 @@ class UserService {
     // }
 
     deleteUser() {
-        return axios.delete("http://localhost:8181/userpage/delete/{user_id}");
+        return axios.delete("https://backendecommerce.azurewebsites.net/userpage/delete/{user_id}");
     }
 
 
     update(id,user){
-        return axios.put(`http://localhost:8181/userpage/update/${id}`, user);
+        return axios.put(`https://backendecommerce.azurewebsites.net/userpage/update/${id}`, user);
     }
 
     createUser(user){
-        return axios.post(`http://localhost:8181/userpage/save`, user);
+        return axios.post(`https://backendecommerce.azurewebsites.net/userpage/save`, user);
     }
 
     checkUser(email){
-        return axios.get(`http://localhost:8181/userpage/check/${email}`).then(response=>response.data);
+        return axios.get(`https://backendecommerce.azurewebsites.net/userpage/check/${email}`).then(response=>response.data);
     }
 
     getUserById(id){
-        return axios.get(`http://localhost:8181/userpage/show/${id}`);
+        return axios.get(`https://backendecommerce.azurewebsites.net/userpage/show/${id}`);
     }
 
     getUserByEmail(email){
-        return axios.get(`http://localhost:8181/userpage/userbyemail/${email}`);
+        return axios.get(`https://backendecommerce.azurewebsites.net/userpage/userbyemail/${email}`);
     }
 
 }
