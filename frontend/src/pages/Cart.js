9e62b24ce9dc4cadd19c070d6d9a4c47a2582546
Kenzie <kenzieli23@gmail.com>
2023-01-18 13:55:42 -0600

    update to cart page

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ProductService from "../services/ProductService";
-import Nav from "../components/Navbar/PageWrapper";
+import axios from 'axios';
 import {
   MDBBtn,
   MDBCard,
@@ -17,18 +16,21 @@ import {
   MDBRow,
   MDBTooltip,
   MDBTypography,
-  } from "mdb-react-ui-kit";
-
+} from "mdb-react-ui-kit";
+import UserService from "../services/UserService";
+import AddressService from "../services/AddressService";
+import useAuthUser from "../hook/getUser";
 
 
 const Cart = () => {
-  const [price, setPrice] = useState(null);
   const [items,setItems] = useState([]);
-  const [product,setProduct] = useState("");
   var chosenItems = [];
   var cartItem = [];
   const[totalPrice, setTotalPrice]  = useState(0);
-  const[num, setNum] = useState(0);
+  const [user, setUser] = useState("");
+  const [address, setAddress] = useState("");
+  const [loading, setLoading] = useState(true);
+  const userInfo = useAuthUser();
 
   function addToChosenItems(id, price, quantity){
     chosenItems.push(price);
@@ -50,138 +52,173 @@ const Cart = () => {
       }
       setTotalPrice(numTotal);
     }
-    }, [items.length]);   
+    const fetchData  = async () => {//GET CURRENT USER AND THEIR ADDRESS 
+      try { 
+        setLoading(true);  
+        const email = JSON.parse(localStorage.getItem("userEmail"));       
+        const response = await UserService.getUserByEmail(email);  
+        setUser(response.data);   
+        const result = await AddressService.findAllAddresses();
+        setAddress(result.data.filter(a=>{return a.userId.userId === response.data.userId}));
+        console.log(user); //testing
+        console.log(address);//testing
+      } catch(error) {
+          console.log(error);
+      }
+      setLoading(false);   
+  }; 
+
+  fetchData();
+    }, [items.length]);
+
+    //MAIL TEMPLATE
+    const mailTemplate = {//HOW TO GET THE USER'S ADDRESS HERE?
+      recipient: user.email,
+      msgBody: `
+        User ${user.username}, thank you for your purchase. 
+        Your order will be delivered to ${address}.
+        You can track your order here: {tracking goes here}.
+      `,
+      subject: "Thank you for your purchase!"
+    }
+    //EMAILING FUNCTION
+    const sendEmail = async (event) => {
+      axios({ //connect to backend mailer
+        method: "POST",
+        url:"http://localhost:8181/email/send",
+        data:  mailTemplate
+      })
+    }
 
   return (
+    <>
+    <section className="h-100 h-custom" style={{ backgroundColor: "#fdddc3" }}>
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center my-4">
+          <MDBCol md="8">
+            <MDBCard className="mb-4">
+              <MDBCardHeader className="py-3">
+              <MDBTypography tag="h1" className="fw-bold mb-0 text-black">
+                        Cart
+                      </MDBTypography>
+              </MDBCardHeader>
+              {items.map(
+            ({productId, productName, pricePerUnit, productImg, productDescription}) => (
+              <MDBCardBody key={productId}>
+                <MDBRow>
+                  <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
+                  <MDBRipple rippleTag="div" rippleColor="light"
+                    className="bg-image rounded hover-zoom hover-overlay"> 
+                    <img src={productImg} className = "w-100" />
+                    
+                    </MDBRipple>
+                  </MDBCol>
 
-  <section className="h-100 h-custom" style={{ backgroundColor: "#fdddc3" }}>
-    <MDBContainer className="py-5 h-100">
-      <MDBRow className="justify-content-center my-4">
-        <MDBCol md="8">
-          <MDBCard className="mb-4">
-            <MDBCardHeader className="py-3">
-            <MDBTypography tag="h1" className="fw-bold mb-0 text-black">
-                      Cart
-                    </MDBTypography>
-            </MDBCardHeader>
-            {items.map(
-          ({productId, productName, pricePerUnit, productImg, productDescription}) => (
-            <MDBCardBody key={productId}>
-              <MDBRow>
-                <MDBCol lg="3" md="12" className="mb-4 mb-lg-0">
-                <MDBRipple rippleTag="div" rippleColor="light"
-                  className="bg-image rounded hover-zoom hover-overlay"> 
-                  <img src={productImg} className = "w-100" />
+                  <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
+                    <p>
+                      <strong>{productName}</strong>
+                    </p>
+                    <p>{productDescription}</p>
                   
-                  </MDBRipple>
                 </MDBCol>
 
-                <MDBCol lg="5" md="6" className=" mb-4 mb-lg-0">
-                  <p>
-                    <strong>{productName}</strong>
+                <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
+                  <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
+                    <MDBBtn className="px-3 me-2">
+                      <MDBIcon fas icon="minus" />
+                    </MDBBtn>
+
+                    <MDBInput defaultValue={1} min={0} type="number" label="Quantity" />
+
+                    <MDBBtn className="px-3 ms-2">
+                      <MDBIcon fas icon="plus" />
+                    </MDBBtn>
+                  </div>
+
+                  <p className="text-start text-md-center">
+                    <strong>${pricePerUnit}</strong>
+                    <p>{addToChosenItems(productId, pricePerUnit, "1")}</p>
                   </p>
-                  <p>{productDescription}</p>
-                
-              </MDBCol>
 
-              <MDBCol lg="4" md="6" className="mb-4 mb-lg-0">
-                <div className="d-flex mb-4" style={{ maxWidth: "300px" }}>
-                  <MDBBtn className="px-3 me-2">
-                    <MDBIcon fas icon="minus" />
-                  </MDBBtn>
+                  <button onClick={(e)=>deleteProduct(productId,e)} className="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i className="fa fa-trash"></i></button>
 
-                  <MDBInput defaultValue={1} min={0} type="number" label="Quantity" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            )
+            )}
+            </MDBCard>      
+          </MDBCol>
+        <MDBCol md="4">
+          <MDBCard className="mb-4">
+            <MDBCardHeader>
+              <MDBTypography tag="h5" className="mb-0">
+                Order Summary
+              </MDBTypography>
+            </MDBCardHeader>
+            <MDBCardBody>
+              <MDBListGroup flush>
+                <MDBListGroupItem
+                  className="d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                  Products
+                  <span>PRODUCTS + PRICE HERE</span>
+                </MDBListGroupItem>
+                <MDBListGroupItem className="d-flex justify-content-between align-items-center px-0">
+                  Shipping
+                  <span>$0.00 (Free Shipping)</span>
+                </MDBListGroupItem>
+                <MDBListGroupItem
+                  className="d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                  <div>
+                    <strong>Order Total</strong>
+                    <strong>
+                      <p className="mb-0">(including VAT)</p>
+                    </strong>
+                  </div>
+                  <span>
+                    <strong>${totalPrice}</strong>
+                  </span>
+                </MDBListGroupItem>
+              </MDBListGroup>
 
-                  <MDBBtn className="px-3 ms-2">
-                    <MDBIcon fas icon="plus" />
-                  </MDBBtn>
-                </div>
-
-                <p className="text-start text-md-center">
-                  <strong>${pricePerUnit}</strong>
-                  <p>{addToChosenItems(productId, pricePerUnit, "1")}</p>
-                </p>
-
-                <button onClick={(e)=>deleteProduct(productId,e)} className="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i className="fa fa-trash"></i></button>
-
-                </MDBCol>
-              </MDBRow>
+              <MDBBtn block size="lg" onClick={sendEmail}>
+                CHECKOUT
+              </MDBBtn>
             </MDBCardBody>
-          )
-          )}
-          </MDBCard>      
+          </MDBCard>
+
+          <MDBCard className="mb-4">
+            <MDBCardBody>
+              <p>
+                <strong>Expected shipping delivery</strong>
+              </p>
+              <p className="mb-0">Will be updated after checkout</p>
+            </MDBCardBody>
+          </MDBCard>
+          <MDBCard className="mb-4 mb-lg-0">
+            <MDBCardBody>
+              <p>
+                <strong>We accept</strong>
+              </p>
+              <MDBCardImage className="me-2" width="45px"
+                src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/visa.svg"
+                alt="Visa" />
+              <MDBCardImage className="me-2" width="45px"
+                src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/amex.svg"
+                alt="American Express" />
+              <MDBCardImage className="me-2" width="45px"
+                src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/mastercard.svg"
+                alt="Mastercard" />
+              <MDBCardImage className="me-2" width="45px"
+                src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce/includes/gateways/paypal/assets/images/paypal.png"
+                alt="PayPal acceptance mark" />
+            </MDBCardBody>
+          </MDBCard>
         </MDBCol>
-      <MDBCol md="4">
-        <MDBCard className="mb-4">
-          <MDBCardHeader>
-            <MDBTypography tag="h5" className="mb-0">
-              Order Summary
-            </MDBTypography>
-          </MDBCardHeader>
-          <MDBCardBody>
-            <MDBListGroup flush>
-              <MDBListGroupItem
-                className="d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Products
-                <span>PRODUCTS + PRICE HERE</span>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center px-0">
-                Shipping
-                <span>$0.00 (Free Shipping)</span>
-              </MDBListGroupItem>
-              <MDBListGroupItem
-                className="d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                <div>
-                  <strong>Order Total</strong>
-                  <strong>
-                    <p className="mb-0">(including VAT)</p>
-                  </strong>
-                </div>
-                <span>
-                  <strong>${totalPrice}</strong>
-                </span>
-              </MDBListGroupItem>
-            </MDBListGroup>
-
-            <MDBBtn block size="lg">
-              CHECKOUT
-            </MDBBtn>
-          </MDBCardBody>
-        </MDBCard>
-
-        <MDBCard className="mb-4">
-          <MDBCardBody>
-            <p>
-              <strong>Expected shipping delivery</strong>
-            </p>
-            <p className="mb-0">Will be updated after checkout</p>
-          </MDBCardBody>
-        </MDBCard>
-        <MDBCard className="mb-4 mb-lg-0">
-          <MDBCardBody>
-            <p>
-              <strong>We accept</strong>
-            </p>
-            <MDBCardImage className="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/visa.svg"
-              alt="Visa" />
-            <MDBCardImage className="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/amex.svg"
-              alt="American Express" />
-            <MDBCardImage className="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/mastercard.svg"
-              alt="Mastercard" />
-            <MDBCardImage className="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce/includes/gateways/paypal/assets/images/paypal.png"
-              alt="PayPal acceptance mark" />
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-  </MDBContainer>
-</section>
-
-    
+      </MDBRow>
+    </MDBContainer>
+  </section>
+  </>
   )
 };
 
