7acf7b6aad87a3b7e9040366485afe9d34f017cc
phuonghhc <116579412+phuonghhc@users.noreply.github.com>
2023-01-18 10:12:05 -0600

    First commit
    It runs after configuring

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,11 @@
-export const oktaConfig = {
- issuer: 'https://dev-38245936.okta.com/oauth2/default',
-    clientId: "0oa7urjx6kd5LTbMZ5d7",   //chinar's clientId
 
+export const oktaConfig = {
+
+    //dev-09055718.okta.com/oauth2/default',
+    issuer: 'https://dev-97900310.okta.com/oauth2/default',
+    //clientId: "0oa7qou2xcRLl9WvU5d7",
+    clientId: "0oa7sq1asxXKBblPa5d7", // SINGLE PAGE APP - WORKS
+    //clientId: "0oa7spieo4luUqayk5d7", WEB APP - DOESN'T WORK
     redirectUri: `${window.location.origin}/login/callback`,
     //redirectUri: 'http://localhost:3000/login/callback',
     endSessionRedirectUri: `${window.location.origin}`,
@@ -9,3 +13,18 @@ export const oktaConfig = {
     pkce: true,
     disableHttpsCheck: true,
 };
+
+
+
+
+// export const oktaConfig = {
+//  issuer: 'https://dev-38245936.okta.com/oauth2/default',
+//     clientId: "0oa7urjx6kd5LTbMZ5d7",   //chinar's clientId
+
+//     redirectUri: `${window.location.origin}/login/callback`,
+//     //redirectUri: 'http://localhost:3000/login/callback',
+//     endSessionRedirectUri: `${window.location.origin}`,
+//     scopes: ["openid", "profile", "email"],
+//     pkce: true,
+//     disableHttpsCheck: true,
+// };
