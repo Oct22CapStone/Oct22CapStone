9f7a418ed198887a1ff2bb39eeeb03ae3aebdc27
phuonghhc <116579412+phuonghhc@users.noreply.github.com>
2023-01-18 09:02:43 -0600

    last commit

--- backend/src/main/java/com/mtumer/BackendApplication.java
+++ backend/src/main/java/com/mtumer/BackendApplication.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.mtumer.email.EmailService;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApplication{
 
 	
