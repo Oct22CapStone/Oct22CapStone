a92553e25710c26fdbc7425a5d21286045f23d1a
kenzkenzkenz <89564373+kenzkenzkenz@users.noreply.github.com>
2023-01-24 21:45:58 -0600

    Update PageWrapper.js
    added a delay for the data loading. -Kenzie

--- frontend/src/components/Navbar/PageWrapper.js
+++ frontend/src/components/Navbar/PageWrapper.js
@@ -14,15 +14,11 @@ const Nav = () => {
     const userInfo = useAuthUser();
     const [loading, setLoading] = useState(true);
     const [isAdmin, setIsAdmin] = useState(false);
+
     function refreshPage() {
         window.location.reload(true);
     }
-    function Admin() {
-        if (isAdmin) {
-            return (
-                <li className="nav-item dropdown"> <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">                        <li><Link className="dropdown-item" to="/viewproducts">Manage Products</Link></li>                        <li><Link className="dropdown-item" to="/orders">Manage Orders</Link></li>                        <li><Link className="dropdown-item" to="/viewuser">Manage Users</Link></li>                    </ul>                </li>)
-        }
-    }
+
     window.updateCartTotal = function () {
         if (JSON.parse(localStorage.getItem('cart')) != null) {
             setItems((JSON.parse(localStorage.getItem('cart'))).length);
@@ -33,85 +29,124 @@ const Nav = () => {
         oktaAuth.tokenManager.clear(oktaAuth.getIdToken());
         oktaAuth.closeSession();
         localStorage.setItem("okta-shared-transaction-storage", "");
-        localStorage.setItem("cart","[]");
+        localStorage.clear();
         refreshPage();
     };
-    useEffect(() => {
-        if (JSON.parse(localStorage.getItem('cart')) != null) {
-            const cart = JSON.parse(localStorage.getItem("cart")).length;
-            setCartItems(cart);
-        }
-    }, [cartItems]);
+    // useEffect(() => {
+    //     if (JSON.parse(localStorage.getItem('cart')) != null) {
+    //         const cart = JSON.parse(localStorage.getItem("cart")).length;
+    //         setCartItems(cart);
+    //     }
+    // }, [cartItems]);
     useEffect(() => {
         const fetchData = async () => {
+            //setLoading(true);
             const email = JSON.parse(localStorage.getItem("userEmail"));
             const userRes = await UserService.getUserByEmail(email);
             const roleRes = await UserRoleService.findAllUserRole();
             var roles = roleRes.data.filter(a => { return a.user.userId === userRes.data.userId }).
                 map(function (r) { return r.role.roleId });
-            console.log(roles);
             if (roles == 1) {
                 setIsAdmin(true);
             } else {
                 setIsAdmin(false);
             }
+            //setLoading(false);
         }
         fetchData();
         if (JSON.parse(localStorage.getItem('cart')) != null) {
             setItems((JSON.parse(localStorage.getItem('cart'))).length);
         }
+
+        //SET A TIMEOUT FOR PROPER LOADING OF USER'S DATA
+        //see https://reactgo.com/settimeout-in-react-hooks/ for info
+        setTimeout(() => fetchData(), 1500);//KENZIE
+        const timer = setTimeout(() => console.log('Initial timeout!'), 1000);//KENZIE
+        clearTimeout(timer);//KENZIE
+
     }, [global.change]);
     return (
+        // <>{!loading && (
         <nav className="navbar navbar-expand-lg navbar-light bg-light navbar-collapse white-space-nowrap">
             <div className="container-fluid px-4 px-lg-5">
-            <a href="/"><img src="https://res.cloudinary.com/db5fpphyj/image/upload/v1674150708/CapstonePageWrapperImages/vehiclevault_logo_o19ase.jpg" className= "float-left" alt="logo3" border="0" width = "400" height = "100" align ="left"></img></a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                    <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
-                    <li className="nav-item"><Link to="/about" className="nav-link active" aria-current="page">About</Link></li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="/">All Products</a></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><Link className="dropdown-item" to="/popularitems">Highlighted Items</Link></li>
-                            <li><Link className="dropdown-item" to="/latestproducts">New Arrivals</Link></li>
+                <a href="/"><img src="https://res.cloudinary.com/db5fpphyj/image/upload/v1674150708/CapstonePageWrapperImages/vehiclevault_logo_o19ase.jpg" className="float-left" alt="logo3" border="0" width="400" height="100" align="left"></img></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                        <li className="nav-item"><Link to="/about" className="nav-link active" aria-current="page">About</Link></li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a className="dropdown-item" href="/">All Products</a></li>
+                                <li><hr className="dropdown-divider" /></li>
+                                <li><Link className="dropdown-item" to="/popularitems">Highlighted Items</Link></li>
+                                <li><Link className="dropdown-item" to="/latestproducts">New Arrivals</Link></li>
                             </ul> </li></ul>
-                            <form className="d-flex"> 
-                            <Link to="/cart" className="btn btn-outline-dark" type="submit">
-                                <i className="bi-cart-fill me-1"></i>Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">{items}</span> 
-                                </Link></form>
-                                <ul className="nav navbar-nav ms-auto mb-2 mb-lg-0 ms-lg-4 nav-fill">
-                                    <Admin />
-                                    {
+                    <form className="d-flex">
+                        <Link to="/cart" className="btn btn-outline-dark" type="submit">
+                            <i className="bi-cart-fill me-1"></i>Cart
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">{items}</span>
+                        </Link></form>
+                    <ul className="nav navbar-nav ms-auto mb-2 mb-lg-0 ms-lg-4 nav-fill">
+                        {/* <Admin /> */}
 
-                                    authState?.isAuthenticated ? (
-                                    <div className="ms-auto mb-lg-0"> 
+                        {/* IF LOGGED IN SHOW CREATE NEW USER, OTHERWISE SHOW REGISTER */}
+                        {
+                            authState?.isAuthenticated && isAdmin == true ? (
+                                <div className="ms-auto mb-lg-0">
                                     <li className="nav-item"><Link to="/register" className="nav-link active" aria-current="page" >Create New Account</Link></li>
-                                    </div>) : (
-                                        <div className="ms-auto mb-lg-0">
-                                            <li className="nav-item"><Link to="/register" className="nav-link active" aria-current="page" >Register</Link></li>
-                                            </div>)
-                                            
-            }
-                {
-                    authState?.isAuthenticated ? (
-                        <div className="ms-auto mb-lg-0">                                    <li className="nav-item"><a className="nav-link active" aria-current="page" href="/profile">Welcome {userInfo?.given_name}!</a></li>                                </div>) : (
-                        <div className="ms-auto mb-lg-0">                                </div>)
-                }
-                {
-                    authState?.isAuthenticated ? (
-                        <div className="ms-auto mb-lg-0">                                    <li className="nav-item"><img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png" alt="Admin" class="rounded-circle" width="40"></img></li>                                </div>) : (
-                        <div className="ms-auto mb-4 mb-lg-0 mb-4">                                </div>)
-                }
-                {
-                    authState?.isAuthenticated ? (
-                        <div className="ms-auto mb-lg-0">                                    <button className="btn btn-link active" onClick={loggingOut}>Logout</button>                                </div>) : (
-                        <div className="ms-auto mb-lg-0">                                    <Link className="btn-link btn active" to="/login">Login</Link>                                </div>)
-                }
-                {/*<li className="nav-item"><a className="nav-link active" aria-current="page" onClick={loggingIn}>Login</a></li>*/}
-            </ul>                </div>            </div>        </nav>);
+                                    <li className="nav-item dropdown"> <a className="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>
+                                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">                        
+                                        <li><Link className="dropdown-item" to="/viewproducts">Manage Products</Link></li>                        
+                                        <li><Link className="dropdown-item" to="/orders">Manage Orders</Link></li>
+                                        <li><Link className="dropdown-item" to="/viewuser">Manage Users</Link></li>
+                                    </ul>
+                </li>
+                                </div>
+                                ) : (
+                                <div className="ms-auto mb-lg-0">
+                                    {/* <li className="nav-item"><Link to="/register" className="nav-link active" aria-current="page" >Register</Link></li> */}
+                                </div>)
+
+                        }
+
+                        {/* IF LOGGED IN SAY WELCOME USER! OTHERWISE, NOTHING */}
+                        {
+                            authState?.isAuthenticated ? (
+                                <div className="ms-auto mb-lg-0">            
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="/profile">Welcome {userInfo?.given_name}!</a>
+                                </li>
+                                </div>
+                                ) : (
+                                <div className="ms-auto mb-lg-0">
+                                </div>)
+                        }
+
+                        {/* IF LOGGED IN SHOW ICON, OTHERWISE, DO NOT */}
+                        {
+                            authState?.isAuthenticated ? (
+                                <div className="ms-auto mb-lg-0">
+                                    <li className="nav-item"><img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png" className="rounded-circle" width="40"></img></li>                                
+                                </div>
+                                    ) : (
+                                <div className="ms-auto mb-4 mb-lg-0 mb-4">
+                                </div>)
+                        }
+                        {/* IF LOGGED IN SHOW LOG OUT BUTTON, OTHERWISE SHOW LOG IN BUTTON */}
+                        {
+                            authState?.isAuthenticated ? (
+                                <div className="ms-auto mb-lg-0"><button className="btn btn-link active" onClick={loggingOut}>Logout</button>                                </div>) : (
+                                <div className="ms-auto mb-lg-0"><Link className="btn-link btn active" to="/login">Login</Link>                                </div>)
+                        }
+                        {/*<li className="nav-item"><a className="nav-link active" aria-current="page" onClick={loggingIn}>Login</a></li>*/}
+                    </ul>                
+                    </div>            
+                    </div>        
+                    </nav>
+    // )}
+    // </>
+                );
 };
 export default Nav;
