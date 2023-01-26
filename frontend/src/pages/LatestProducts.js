52ab6ed71d4b320a87fdaea59f41e45f64fdd29f
chinar97 <120409366+chinar97@users.noreply.github.com>
2023-01-26 16:11:17 -0500

    works

--- frontend/src/pages/LatestProducts.js
+++ frontend/src/pages/LatestProducts.js
@@ -116,7 +116,7 @@ const LatestProducts = () => {
     };
     
     useEffect(() => {
-        fetch(`https://vehiclevault.azurewebsites.net/product/${num}`)
+        fetch(`https://backendecommerce.azurewebsites.net/product/${num}`)
             .then((res) => res.json())
             .then((data) => {
                 setProduct(data);
